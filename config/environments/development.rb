622be9d9c13b09df665a40a9b4a6fe812d818fb3
Pete Hawkins <petey@hey.com>
2021-03-03 22:56:17 +0000

    style: reset password styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
 end
