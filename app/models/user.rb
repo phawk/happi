9ccb06b528f1939e2d9acffc5d15e9b0a8a2c209
Pete Hawkins <petey@hey.com>
2021-03-03 21:06:52 +0000

    feat(models): add base models

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :trackable,
+         :recoverable, :rememberable, :validatable
+
+  has_and_belongs_to_many :teams
+  belongs_to :team, optional: true
+end
