f7a3a1ebf48fe9f84b82462a8908d3d3ad2d6a1e
Pete Hawkins <petey@hey.com>
2021-03-03 22:38:23 +0000

    feat(auth): get sign up working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
 end
