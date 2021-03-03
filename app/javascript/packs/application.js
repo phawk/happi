f216d143fee77b6fbcfa1813fd91b5d312c4b1e8
Pete Hawkins <petey@hey.com>
2021-03-03 20:55:06 +0000

    feat(turbo): install turbo and stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,11 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+
+import "controllers"
