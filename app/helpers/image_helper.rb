6d8d16f208184ca9952169ec8e920a1a646cc6e3
Pete Hawkins <petey@hey.com>
2021-03-03 22:50:21 +0000

    style(layout): work on nav style

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,7 +1,7 @@
 module ImageHelper
   def avatar_url(user, size: 32)
     name = [user.first_name, user.last_name].join("%20")
-    "https://eu.ui-avatars.com/api/?name=#{name}&size=#{size*2}"
+    "https://eu.ui-avatars.com/api/?name=#{name}&size=#{size*2}&background=FFE299&color=000"
   end
 
   def avatar_tag(user, size: 32, extra_class: "")
