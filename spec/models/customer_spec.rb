75d0a60133e00ef025c303a2a35e28573a6a259b
Pete Hawkins <petey@hey.com>
2021-03-04 11:38:27 +0000

    feat(customers): form to manually add new ones

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,4 +3,8 @@ require "rails_helper"
 RSpec.describe Customer, type: :model do
   it { is_expected.to belong_to(:team) }
   it { is_expected.to have_many(:message_threads) }
+
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
+  it { is_expected.to validate_presence_of(:email) }
 end
