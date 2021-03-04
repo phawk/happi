fa878f8677014c5ce00f1f17ab62c3f4b0e0e1b0
Pete Hawkins <petey@hey.com>
2021-03-04 08:44:44 +0000

    feat(seeds): setup v. basic DB seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Set up your user and team before running seeds
+
+user = User.first
+team = Team.first
+
+alfonso = Customer.where(
+  team: team,
+  first_name: "alfonso",
+  last_name: "ayala",
+  email: "casamiaus2020@gmail.com",
+  company: "Glide programer",
+  country_code: "MX"
+).first_or_create!
+
+alfonso_thread_1 = MessageThread.where(
+  team: team,
+  customer: alfonso,
+  subject: "Contact form submission"
+).first_or_create!
+
+Message.where(
+  message_thread: alfonso_thread_1,
+  sender: alfonso,
+  body: "Hi, I'm from Mexico, it also works with Mexican banks.?",
+  status: "received"
+).first_or_create!
