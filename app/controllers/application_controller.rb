5ace0693777347923db3c1db59f9f468ec1a24b9
Pete Hawkins <petey@hey.com>
2021-03-04 14:22:55 +0000

    feat(avatars): allow avatars to be uploaded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name avatar])
   end
 end
