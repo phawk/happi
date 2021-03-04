901442e2a3469bb9a8d768c6ec03a5d8cdd5d33a
Pete Hawkins <petey@hey.com>
2021-03-04 12:17:43 +0000

    feat(colourable): add to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Colourable
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
