3979a7ef874ca3563f8c8529dd00b15abcd5718d
Pete Hawkins <petey@hey.com>
2021-03-04 11:38:27 +0000

    feat(customers): form to manually add new ones

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,11 +4,36 @@ class CustomersController < ApplicationController
   end
 
   def show
+    @customer = current_team.customers.find(params[:id])
   end
 
   def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = current_team.customers.new(customer_params)
+
+    if @customer.save
+      redirect_to @customer, notice: "Customer saved."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :name,
+      :email,
+      :company,
+      :phone,
+      :country_code,
+      :location
+    )
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,5 @@ class Customer < ApplicationRecord
 
   belongs_to :team
   has_many :message_threads, dependent: :destroy
+  validates :first_name, :last_name, :email, presence: true
 end
