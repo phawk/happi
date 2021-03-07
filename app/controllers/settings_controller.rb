61fb74aa924dda066ac5f6a603f8c537b19544a2
Pete Hawkins <petey@hey.com>
2021-03-07 11:55:52 +0000

    feat(custom emails): allow adding in settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,4 +1,6 @@
 class SettingsController < ApplicationController
   def show
+    @custom_emails = current_team.custom_email_addresses
+    @custom_email_address = CustomEmailAddress.new
   end
 end
