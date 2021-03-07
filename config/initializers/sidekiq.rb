efa1e69c4999b8a73d53696095be5c6322b1b1cc
Pete Hawkins <petey@hey.com>
2021-03-07 17:28:59 +0000

    feat(sidekiq): set up

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,17 @@
+require "sidekiq/web"
+
+# Don't configure redis in testing
+unless Rails.env.test?
+  redis_settings = {
+    url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/4"),
+    network_timeout: 3,
+  }
+
+  Sidekiq.configure_server do |config|
+    config.redis = redis_settings
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = redis_settings
+  end
+end
