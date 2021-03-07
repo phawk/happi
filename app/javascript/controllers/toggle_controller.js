bd6c6b3d8d6330407fc56b8e0538044f5b090def
Pete Hawkins <petey@hey.com>
2021-03-07 20:57:28 +0000

    fix(toggle): remove group complexity not needed

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -9,13 +9,9 @@ import { useClickOutside } from "stimulus-use";
  *
  * Action:
  * data-action="toggle#toggle"
- * Optionally add a group to perform toggle on subset of targets...
- * data-toggle-group="menu"
  *
  * Targets:
  * data-toggle-target="toggleable" data-css-class="class-to-toggle"
- * Optionally add a group when using group with action...
- * data-toggle-group="menu"
  *
  */
 export default class extends ApplicationController {
@@ -28,14 +24,8 @@ export default class extends ApplicationController {
   toggle(e) {
     e.preventDefault();
 
-    const { toggleGroup } = e.currentTarget.dataset;
-
     this.toggleableTargets.forEach((target) => {
-      if (toggleGroup && target.dataset.toggleGroup === toggleGroup) {
-        target.classList.toggle(target.dataset.cssClass);
-      } else if (!toggleGroup) {
-        target.classList.toggle(target.dataset.cssClass);
-      }
+      target.classList.toggle(target.dataset.cssClass);
     });
   }
 
