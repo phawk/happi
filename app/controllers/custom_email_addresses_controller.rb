8b78663b7e19fcdf7416f8a63fc4da4b95953ecd
Pete Hawkins <petey@hey.com>
2021-03-07 13:33:01 +0000

    feat(custom emails): allow removing

--- app/controllers/custom_email_addresses_controller.rb
+++ app/controllers/custom_email_addresses_controller.rb
@@ -3,13 +3,16 @@ class CustomEmailAddressesController < ApplicationController
     @custom_email_address = current_team.custom_email_addresses.new(custom_email_params)
 
     if @custom_email_address.save
-      redirect_to settings_path, notice: "#{@custom_email_address.email} added"
+      redirect_to settings_path, notice: t(".success", email: @custom_email_address.email)
     else
       render partial: "form", status: :unprocessable_entity
     end
   end
 
   def destroy
+    custom_email = current_team.custom_email_addresses.find(params[:id])
+    custom_email.destroy
+    redirect_to settings_path, notice: t(".removed", email: custom_email.email)
   end
 
   private
