d0aa541ed3c62bec88d636323f4c640ac06745e6
Pete Hawkins <petey@hey.com>
2021-03-07 16:36:41 +0000

    feat(mailers): style the mailer layout

---
+++ spec/mailers/previews/devise_preview.rb
@@ -0,0 +1,13 @@
+class DevisePreview < ActionMailer::Preview
+  def reset_password_instructions
+    Devise::Mailer.reset_password_instructions(User.first, "fake-token")
+  end
+
+  def email_changed
+    Devise::Mailer.email_changed(User.first, {})
+  end
+
+  def password_change
+    Devise::Mailer.password_change(User.first, {})
+  end
+end
