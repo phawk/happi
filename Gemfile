4022d26c195f6200b4ba0295f40da908ab6d50ac
Pete Hawkins <petey@hey.com>
2021-03-07 16:02:01 +0000

    feat(mailers): add Roadie

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem "name_of_person"
 gem "chroma"
 gem "aws-sdk-s3", require: false
 gem "postmark-rails"
+gem "roadie-rails"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,22 @@
 class ApplicationMailer < ActionMailer::Base
+  include Roadie::Rails::Automatic
+
   default from: 'yo@happi.team'
   layout 'mailer'
+
+  rescue_from Postmark::InvalidMessageError do |e|
+    matches = e.message.match(/addresses: ([^\s]+)/)
+    email = matches[1].sub(/\.\z/, "")
+    Rails.logger.info("=" * 100)
+    Rails.logger.info("Postmark failed to send to: #{email}")
+    Rails.logger.info("=" * 100)
+    # User.find_by!(email: email).update(email_bounced: true)
+  rescue StandardError
+  end
+
+  private
+
+  def roadie_options
+    super unless Rails.env.test?
+  end
 end
