c56b49a77c838f480ac131410cbde9eca792a617
Pete Hawkins <petey@hey.com>
2021-03-13 07:55:31 +0000

    feat(flash): auto dismiss after 3 secs

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,10 +1,20 @@
 import ApplicationController from "./application_controller";
 
 export default class extends ApplicationController {
+  connect() {
+    setTimeout(() => {
+      this.hideAlert();
+    }, 3000);
+  }
+
   dismiss(event) {
     event.preventDefault();
     event.stopPropagation();
 
+    this.hideAlert();
+  }
+
+  hideAlert() {
     this.element.style.display = "none";
   }
 }
