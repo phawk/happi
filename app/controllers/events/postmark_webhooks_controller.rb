3796ea761f9fa7f1e5fc1ffccab49fd8949e0d84
Pete Hawkins <petey@hey.com>
2021-04-25 12:46:15 +0100

    feat(postmark): add basic auth to postmark API endpoint

--- app/controllers/events/postmark_webhooks_controller.rb
+++ app/controllers/events/postmark_webhooks_controller.rb
@@ -3,6 +3,10 @@ class Events::PostmarkWebhooksController < ApplicationController
   skip_before_action :ensure_team!
 
   def create
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'postmark' && password == 'h4pp1t1m35'
+    end
+
     case params["RecordType"]
     when "Delivery"
       if (message_id = params.dig("Metadata", "message_id"))
--- spec/requests/events/postmark_webhooks_request_spec.rb
+++ spec/requests/events/postmark_webhooks_request_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Events::PostmarkWebhooksController, type: :request do
   let(:message) { messages(:payhere_alex_password_reset_msg_1) }
+  let(:http_auth) do
+    ActionController::HttpAuthentication::Basic.encode_credentials("postmark", "h4pp1t1m35")
+  end
 
   describe "#create" do
     it "marks message as delivered" do
@@ -17,7 +20,7 @@ RSpec.describe Events::PostmarkWebhooksController, type: :request do
          },
         "RecordType" => "Delivery",
         "MessageStream" => "outbound"
-      }
+      }, headers: { "HTTP_AUTHORIZATION" => http_auth }
 
       message.reload
 
@@ -26,7 +29,7 @@ RSpec.describe Events::PostmarkWebhooksController, type: :request do
     end
 
     it "ignores if message ID not found" do
-      post "/events/postmark", params: { "RecordType" => "Delivery" }
+      post "/events/postmark", params: { "RecordType" => "Delivery" }, headers: { "HTTP_AUTHORIZATION" => http_auth }
 
       expect(response).to have_http_status(:no_content)
     end
