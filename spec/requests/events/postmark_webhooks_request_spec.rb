19d82d8273eb5f57c9e12af7646f663b22d4e7b8
Pete Hawkins <petey@hey.com>
2021-04-25 12:41:46 +0100

    feat(postmark): track delivery status

---
+++ spec/requests/events/postmark_webhooks_request_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Events::PostmarkWebhooksController, type: :request do
+  let(:message) { messages(:payhere_alex_password_reset_msg_1) }
+
+  describe "#create" do
+    it "marks message as delivered" do
+      post "/events/postmark", params: {
+        "MessageID" => "883953f4-6105-42a2-a16a-77a8eac79483",
+        "Recipient" => "john@example.com",
+        "DeliveredAt" => "2019-11-05T16:33:54.9070259Z",
+        "Details" => "Test delivery webhook details",
+        "Tag" => "customer-email",
+        "ServerId" => 23,
+        "Metadata" => {
+          "message_id" => message.id.to_s
+         },
+        "RecordType" => "Delivery",
+        "MessageStream" => "outbound"
+      }
+
+      message.reload
+
+      expect(response).to have_http_status(:no_content)
+      expect(message.status).to eq("delivered")
+    end
+
+    it "ignores if message ID not found" do
+      post "/events/postmark", params: { "RecordType" => "Delivery" }
+
+      expect(response).to have_http_status(:no_content)
+    end
+  end
+end
