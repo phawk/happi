06e354f57ef4dee4a81ec039f3a958f28ca4e2bb
Pete Hawkins <petey@hey.com>
2021-04-25 12:52:06 +0100

    feat(postmark): track bounced emails

--- spec/requests/events/postmark_webhooks_request_spec.rb
+++ spec/requests/events/postmark_webhooks_request_spec.rb
@@ -9,23 +9,30 @@ RSpec.describe Events::PostmarkWebhooksController, type: :request do
   describe "#create" do
     it "marks message as delivered" do
       post "/events/postmark", params: {
-        "MessageID" => "883953f4-6105-42a2-a16a-77a8eac79483",
-        "Recipient" => "john@example.com",
-        "DeliveredAt" => "2019-11-05T16:33:54.9070259Z",
-        "Details" => "Test delivery webhook details",
-        "Tag" => "customer-email",
-        "ServerId" => 23,
         "Metadata" => {
           "message_id" => message.id.to_s
          },
-        "RecordType" => "Delivery",
-        "MessageStream" => "outbound"
+        "RecordType" => "Delivery"
       }, headers: { "HTTP_AUTHORIZATION" => http_auth }
 
       message.reload
 
       expect(response).to have_http_status(:no_content)
       expect(message.status).to eq("delivered")
+    end
+
+    it "marks message as bounced" do
+      post "/events/postmark", params: {
+        "Metadata" => {
+          "message_id" => message.id.to_s
+         },
+        "RecordType" => "Bounce"
+      }, headers: { "HTTP_AUTHORIZATION" => http_auth }
+
+      message.reload
+
+      expect(response).to have_http_status(:no_content)
+      expect(message.status).to eq("bounced")
     end
 
     it "ignores if message ID not found" do
