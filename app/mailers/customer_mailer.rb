92f1db744d06b560e947d65bf3eca44c295fa152
Pete Hawkins <petey@hey.com>
2021-04-25 12:25:09 +0100

    feat(postmark): attach message ID to outbound email metadata

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -6,6 +6,8 @@ class CustomerMailer < ApplicationMailer
     @message_thread = message.message_thread
     @customer = @message_thread.customer
 
+    metadata["message_id"] = @message.id
+
     mail from: message.deliver_email_via, to: @customer.email, reply_to: @message_thread.reply_to_address, subject: @message_thread.subject
   end
 end
