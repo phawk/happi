b460651534eeb0162ccda342f7a10deae52c85bb
Pete Hawkins <petey@hey.com>
2021-05-04 16:01:40 +0100

    feat(settings): split out to sub pages

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,7 +1,12 @@
 class SettingsController < ApplicationController
-  def show
+  def show; end
+
+  def emails
     @custom_emails = current_team.custom_email_addresses
     @custom_email_address = CustomEmailAddress.new
+  end
+
+  def team
     @team_members = current_team.users.to_a
   end
 end
