a4ee3b63a8df027df5caa50bf556980d66dbe060
Pete Hawkins <petey@hey.com>
2021-09-15 18:22:00 +0100

    Send admin alert when new teams are setup

--- spec/requests/teams_request_spec.rb
+++ spec/requests/teams_request_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe "Teams", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "POST /" do
+    it "creates a new team" do
+      perform_enqueued_jobs do
+        post "/teams", params: {
+          team: {
+            name: "Polywork",
+            time_zone: "Eastern Time (US & Canada)",
+            country_code: "US"
+          }
+        }
+
+        expect(response).to redirect_to(dashboard_path)
+        expect(delivered_emails.size).to eq(1)
+        expect(last_email.subject).to eq("Admin alert: A new team needs reviewed")
+      end
+    end
+  end
 end
