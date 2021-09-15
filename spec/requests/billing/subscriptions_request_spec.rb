e9e1081df961958c28772edb35356efe36895878
Pete Hawkins <petey@hey.com>
2021-09-15 15:53:19 +0100

    Spec for billing/success page

--- spec/requests/billing/subscriptions_request_spec.rb
+++ spec/requests/billing/subscriptions_request_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe Billing::SubscriptionsController, type: :request do
       expect(response.body).to include("Business")
     end
   end
+
+  describe "GET /billing/success" do
+    it "renders thank you message" do
+      get billing_success_path
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include("Thanks")
+    end
+  end
 end
