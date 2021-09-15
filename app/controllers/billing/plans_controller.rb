a2dbdc70320bb36ac476f7cec3681a31a65440ef
Pete Hawkins <petey@hey.com>
2021-09-15 13:09:12 +0100

    Add success page to redirect to after payment

--- app/controllers/billing/plans_controller.rb
+++ app/controllers/billing/plans_controller.rb
@@ -1,7 +1,13 @@
 module Billing
   class PlansController < ApplicationController
+    skip_before_action :authenticate_user!, only: :success
+
     def index
       @plans = BillingPlan.all
+    end
+
+    def success
+      render layout: "auth"
     end
   end
 end
