037483eb6ce06f3a4d3eceff2b6f5b287261c932
Pete Hawkins <petey@hey.com>
2021-09-24 11:53:40 +0100

    Update component specs

--- spec/components/empty_message_component_spec.rb
+++ spec/components/empty_message_component_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe EmptyMessageComponent, type: :component do
   it "renders title and body" do
     comp = render_inline(described_class.new(title: "No messages", body: "You have not received a message yet!"))
 
-    expect(comp.css("h3").to_html).to include("No messages")
-    expect(comp.css("p").to_html).to include("You have not received a message yet!")
+    expect(comp.css("h3").text).to include("No messages")
+    expect(comp.css("p").text).to include("You have not received a message yet!")
   end
 end
