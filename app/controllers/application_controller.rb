d65733aefae0156ba71507066130860ef70c9e36
Pete Hawkins <petey@hey.com>
2021-09-24 20:41:11 +0100

    Install sentry for error monitoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   before_action :ensure_team!
+  before_action :sentry_user_info, if: :user_signed_in?
   around_action :set_time_zone, if: :current_user
   after_action :track_page_view
 
@@ -53,5 +54,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name terms_and_conditions])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name avatar])
+  end
+
+  def sentry_user_info
+    Sentry.set_user(email: current_user.email, id: current_user.id)
   end
 end
