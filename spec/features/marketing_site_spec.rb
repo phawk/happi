a0e11c43d58c5547fa5de00bbe55ac1eb73df199
Pete Hawkins <petey@hey.com>
2021-10-02 22:09:44 +0100

    Tidy up marketing site spec

--- spec/features/marketing_site_spec.rb
+++ spec/features/marketing_site_spec.rb
@@ -3,15 +3,12 @@ require "rails_helper"
 RSpec.describe "Marketing site" do
   it "loads without errors" do
     visit root_path
+    expect(page).to have_content "Better customer engagement for startups"
 
-    expect(page).to have_content("Better customer engagement for startups")
-
-    click_on("Get started free")
-
-    expect(page).to have_content("Choose your plan")
+    click_on "Get started free"
+    expect(page).to have_content "Choose your plan"
 
     first(:link, "Get started").click
-
-    expect(page).to have_content("Create your account")
+    expect(page).to have_content "Create your account"
   end
 end
