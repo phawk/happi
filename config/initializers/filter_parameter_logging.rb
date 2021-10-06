e1cc0f26cdc38158fdca9a7f897a51437076f9a4
Pete Hawkins <petey@hey.com>
2021-10-06 18:08:15 +0100

    Add email to filter params

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,5 +2,5 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += %i[
-  passw secret token _key crypt salt certificate otp ssn
+  email passw secret token _key crypt salt certificate otp ssn
 ]
