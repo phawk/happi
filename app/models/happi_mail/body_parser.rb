c2ca17d253a090f710f721c6e66d0f696c002d55
Pete Hawkins <petey@hey.com>
2021-10-08 15:38:57 +0100

    Improve body parser for HTML only emails

--- app/models/happi_mail/body_parser.rb
+++ app/models/happi_mail/body_parser.rb
@@ -3,7 +3,7 @@ module HappiMail
     include ActionView::Helpers::SanitizeHelper
     include ActionView::Helpers::TextHelper
 
-    attr_accessor :found_text_part
+    attr_accessor :found_text_part, :found_html_part
     attr_reader :mail
 
     def initialize(mail)
@@ -13,11 +13,8 @@ module HappiMail
     def content
       resp = EmailReplyParser.parse_reply(text_content)
       resp = simple_format(resp) if found_text_part
+      resp = clean_html(resp) if found_html_part
       resp
-    end
-
-    def has_html?(text)
-      strip_tags(text) != text
     end
 
     def text_content
@@ -34,9 +31,19 @@ module HappiMail
           parts.first.decoded
         end
       else
-        self.found_text_part = true
+        if mail.mime_type == "text/html"
+          self.found_html_part = true
+        else
+          self.found_text_part = true
+        end
         mail.decoded
       end
+    end
+
+    private
+
+    def clean_html(html)
+      sanitize(html).strip
     end
   end
 end
