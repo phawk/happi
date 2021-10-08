d8bd608df7723695ec5724802ae653b1e9d138da
Pete Hawkins <petey@hey.com>
2021-10-08 14:45:51 +0100

    Update Happi customer creation to be an upsert

--- app/services/onboarding_emails_service.rb
+++ app/services/onboarding_emails_service.rb
@@ -8,11 +8,12 @@ module OnboardingEmailsService
   end
 
   def create_happi_customer(team)
-    customer = team.customers.create!(
-      first_name: "Happi",
-      last_name: "Support",
-      email: "yo@happi.team",
-    )
+    customer = team.customers.where(email: "yo@happi.team").first_or_initialize
+    customer.first_name = "Happi"
+    customer.last_name = "Support"
+    customer.country_code = "GB"
+    customer.location = "UK"
+    customer.save!
     customer.avatar.attach(
       io: File.open(icon_path),
       filename: "happi-icon.png",
