a3f400215af5efd6ab7e1590d6d5e4f77d5237dc
Pete Hawkins <petey@hey.com>
2021-10-08 14:22:44 +0100

    Tweak email copy

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -33,8 +33,8 @@ class TeamsController < ApplicationController
 
       session.delete(:signup_plan)
 
-      AdminMailer.notification("A new team needs reviewed",
-        "#{@team.name} just signed up for Happi and needs reviewed.").deliver_later
+      AdminMailer.notification("A new team signed up",
+        "#{@team.name} just signed up for Happi.").deliver_later
 
       billing_plan = BillingPlan.new(name: @team.plan)
       @team.change_plan(billing_plan)
