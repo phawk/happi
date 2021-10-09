c00f2b9c721646872cdfc473c03a52af9f0115b2
Pete Hawkins <petey@hey.com>
2021-10-09 11:04:38 +0100

    Add search to messages

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,4 +3,16 @@ require "rails_helper"
 RSpec.describe Message, type: :model do
   it { is_expected.to belong_to(:message_thread) }
   it { is_expected.to belong_to(:sender) }
+
+  describe ".search" do
+    it "searches message content" do
+      results = described_class.search("Stripe account has been disconnected")
+      expect(results).to contain_exactly(messages(:payhere_alex_stripe_msg_2))
+    end
+
+    it "returns empty when there are no results" do
+      results = described_class.search("notfoundhere")
+      expect(results).to be_empty
+    end
+  end
 end
