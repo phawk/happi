cd0e726b5449999656a190805cc2c0063d3755b5
Pete Hawkins <petey@hey.com>
2021-10-26 21:36:06 +0100

    Now using puma-dev for local development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "happi.test" }
   config.action_mailer.delivery_method = :letter_opener
+
+  config.hosts << "happi.test"
 end
