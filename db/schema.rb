ad447ccc42f83d1203cc2d7d614d035051f8df4e
Pete Hawkins <petey@hey.com>
2022-01-06 17:05:50 +0000

    Update gems for rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_145714) do
+ActiveRecord::Schema.define(version: 2022_01_06_135536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
