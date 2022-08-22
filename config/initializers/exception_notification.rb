6394fd91e72b7339531fcbc8df5ee653bf4a9bfb
Pete Hawkins <petey@hey.com>
2022-08-22 09:40:12 +0100

    Fix rubocop issue

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,9 +1,9 @@
 if Rails.env.production?
   Rails.application.config.middleware.use ExceptionNotification::Rack,
-                                          ignore_exceptions: ["ActionController::InvalidAuthenticityToken"] + ExceptionNotifier.ignored_exceptions,
-                                          email: {
-                                            email_prefix: '[HAPPI] ',
-                                            sender_address: %{"Happi Team" <yo@happi.team>},
-                                            exception_recipients: %w{yo@happi.team}
-                                          }
+    ignore_exceptions: ["ActionController::InvalidAuthenticityToken"] + ExceptionNotifier.ignored_exceptions,
+    email: {
+      email_prefix: "[HAPPI] ",
+      sender_address: %{"Happi Team" <yo@happi.team>},
+      exception_recipients: %w{yo@happi.team},
+    }
 end
