bb5392da6abfb3ad793ea4fc6c55d1a1d1a35d7b
Pete Hawkins <petey@hey.com>
2022-10-14 23:01:51 +0100

    Use different redis URL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@ require "sidekiq/web"
 # Don't configure redis in testing
 unless Rails.env.test?
   redis_settings = {
-    url: ENV.fetch("REDIS_URL", ENV.fetch("REDISCLOUD_URL", "redis://127.0.0.1:6379/4")),
+    url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/4"),
     network_timeout: 3,
   }
 
