32582aa6e683790243e9cdbaa9cecbc9649d4312
Pete Hawkins <petey@hey.com>
2022-10-29 12:16:01 +0100

    Fix rubocop issues

--- spec/requests/team_users_requests_spec.rb
+++ spec/requests/team_users_requests_spec.rb
@@ -5,14 +5,15 @@ FakeCheckoutResponse = Struct.new(:url)
 RSpec.describe "TeamUsers", type: :request do
   let(:user) { users(:pete) }
   let(:team_user) { users(:pete).team_users.find_by(team: user.team) }
+
   before { sign_in(user) }
 
   describe "PUT /team_users/:id" do
     it "allows updating the message_notifications" do
       put "/team_users/#{team_user.id}", params: {
         team_user: {
-          message_notifications: false
-        }
+          message_notifications: false,
+        },
       }
 
       expect(team_user.reload.message_notifications).to be(false)
