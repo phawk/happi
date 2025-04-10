86b6657bccaa136ac02c8cbebbecf84c235a055e
Pete Hawkins <petey@hey.com>
2025-04-10 15:14:47 +0300

    Get it working

--- app/models/knowledge_base/file_upload.rb
+++ app/models/knowledge_base/file_upload.rb
@@ -3,6 +3,6 @@ class KnowledgeBase::FileUpload < ApplicationRecord
   belongs_to :team
 
   has_one_attached :file
-  validates :file, content_type: %w[application/pdf application/vnd.apple.pages application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document image/jpeg image/png image/jpg image/tiff], size: {less_than: 5.megabytes, message: "is too large – must be less than 5mb"}
+  validates :file, content_type: %w[application/pdf application/vnd.apple.pages application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document image/jpeg image/png image/jpg image/tiff text/markdown], size: {less_than: 5.megabytes, message: "is too large – must be less than 5mb"}
   validates :file, presence: true
 end
