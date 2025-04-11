ab79ae2f63c8d527b7da122078f9690f120ecaa9
Pete Hawkins <petey@hey.com>
2025-04-11 09:57:53 +0300

    Add methods to ReplyStatistic for retrieving customer messages and user replies; enhance specs to test new functionality.

--- app/models/reply_statistic.rb
+++ app/models/reply_statistic.rb
@@ -1,4 +1,26 @@
 class ReplyStatistic < ApplicationRecord
   belongs_to :message_thread
   belongs_to :team
+
+  def customer_messages_content
+    messages_from_customer.map { |message| message_content_as_plain_text(message) }.join("\n\n")
+  end
+
+  def user_replies_content
+    replies_from_user.map { |message| message_content_as_plain_text(message) }.join("\n\n")
+  end
+
+  def messages_from_customer
+    message_thread.messages.where(id: message_ids.map(&:to_i))
+  end
+
+  def replies_from_user
+    message_thread.messages.where(id: reply_ids.map(&:to_i))
+  end
+
+  private
+
+  def message_content_as_plain_text(message)
+    message.content.to_plain_text
+  end
 end
--- spec/models/reply_statistic_spec.rb
+++ spec/models/reply_statistic_spec.rb
@@ -1,6 +1,28 @@
 require "rails_helper"
 
 RSpec.describe ReplyStatistic, type: :model do
+  let(:reply_statistic) do
+    ReplyStatistic.create!(
+      message_thread: message_threads(:acme_alex_stripe),
+      message_ids: [messages(:acme_alex_stripe_msg_1).id],
+      reply_ids: [messages(:acme_alex_stripe_msg_2).id],
+      time_to_reply: 7436,
+      team: teams(:acme)
+    )
+  end
+
   it { is_expected.to belong_to(:team) }
   it { is_expected.to belong_to(:message_thread) }
+
+  describe "#customer_messages_content" do
+    it "returns the content of the messages" do
+      expect(reply_statistic.customer_messages_content).to eq("I need to reset my stripe connection, can you help?")
+    end
+  end
+
+  describe "#user_replies_content" do
+    it "returns the content of the messages" do
+      expect(reply_statistic.user_replies_content).to eq("No probs, your Stripe account has been disconnected, you can now connect a new one!")
+    end
+  end
 end
